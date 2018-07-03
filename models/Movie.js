d1fd44503cc7286832217745a8fbefc6fb2b420e
semihdumannn <semihdumannn@gmail.com>
2018-07-04 01:42:13 +0300

    Movie & Director Validation

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,31 @@ const MovieSchema = new Schema({
   director_id : Schema.Types.ObjectId,
   title : {
     type : String,
-    required : true
+    required : [true,'`{PATH}` alanı zorunludur.'],
+    maxlength :[150, '`{PATH}` alanına maksimum `{MAXLENGTH}` karakter giriniz. '],
+    minlength : [5 , '`{PATH}` alanına minimum `{MINLENGTH}` karakter giriniz.']
   },
-  category : String,
-  country : String,
-  year : Number,
-  imdb_score : Number,
+  category : {
+    type : String,
+    maxlength :[150, '`{PATH}` alanına maksimum `{MAXLENGTH}` karakter giriniz. '],
+    minlength : [5 , '`{PATH}` alanına minimum `{MINLENGTH}` karakter giriniz.']
+  },
+  country : {
+    type:String,
+    maxlength :[150, '`{PATH}` alanına maksimum `{MAXLENGTH}` karakter giriniz. '],
+    minlength : [5 , '`{PATH}` alanına minimum `{MINLENGTH}` karakter giriniz.']
+
+  },
+  year : {
+    type : Number,
+    max :2040,
+    min:1900
+  },
+  imdb_score : {
+    type:Number,
+    max :10,
+    min: 0
+  },
   createdAt : {
     type : Date,
     default : Date.now
