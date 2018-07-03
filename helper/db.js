eab69b500988a739d6505d45423e4a32d50fc568
semihdumannn <semihdumannn@gmail.com>
2018-07-03 16:02:12 +0300

    Movie Alt Yapı

---
+++ helper/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+  mongoose.connect('mongodb://movie_user:Semih1453@ds125331.mlab.com:25331/movie-api')
+  .then(()=>{
+    console.log('MongoDb bağlantısı sağlandı');
+  })
+  .catch((err)=>{
+    console.log('Bağlantı Hatası : ',err);
+  });
+
+  mongoose.Promise = global.Promise;
+  
+   
+};
